6f6bde123afc914fd8ba36e62665140f48a100e1
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-03-28 15:38:37 +0300

    adding some script

--- JavaScript /Script1.js
+++ JavaScript /Script1.js
@@ -3,7 +3,11 @@
 var userName = prompt('What is Your name?');
 alert ('Welcom to my site '+userName );
 
+
 var yourTeam = prompt('which team do you support ?!!' ,'Real Madrid , Barcelona , Bayern Munich');
+while (yourTeam !=='Real Madrid' && yourTeam !=='Bayern Munich' && yourTeam !=='Barcelona'){
+    yourTeam= prompt('please enter a team from the list' ,'Real Madrid , Barcelona , Bayern Munich');
+}
 
 if (yourTeam==='Real Madrid'){
     document.write('<img src="Images/RM.jpg" height="200px" width="200px"/>');
@@ -11,6 +15,15 @@ if (yourTeam==='Real Madrid'){
     document.write('<img src="Images/BAYERN.jpg" height="200px" width="200px"/>');
 }else if (yourTeam==='Barcelona'){
     document.write('<img src="Images/Barca.jpg" height="200px" width="200px"/>');
-}else {
-    document.write('<h3>Good Team</h3>');
 }
+var image =''
+var titles =0;
+titles= prompt('how many champions league titles your team have !!');
+for(var i=0;i<titles;i++ ){
+image+='<img src="Images/trophy.jpg" height="200px" width="200px"/>';
+}
+console.log(image);
+document.write(image);
+// else {
+//     document.write('<h3>Good Team</h3>');
+// }
